2ff722f0d74b784ddcc761dc1f3d624a9e5024c6
이동현 <dlehdanakf@konkuk.ac.kr>
2019-10-02 15:30:04 +0900

    연산자 선택 없이 사칙연산 모두 수행되도록 수정

--- Application.java
+++ Application.java
@@ -3,10 +3,11 @@ import ALU.*;
 
 public class Application {
 	public static void main(String[] args) {
+		int a, b;
+		Calculator calc = null;
+		
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-		int a, b, select;
-		Calculator calc = null;
 
 		Application.printHeader();
 		
@@ -30,34 +31,30 @@ public class Application {
 			}
 		}
 		
-		System.out.println("≫ 원하시는 연산자를 선택해주세요.");
-		Application.printExpressionQuestion(a, b);
+		int _case  = 1;
 		while(true) {
-			try {
-				select = Integer.parseInt(scan.nextLine());
-				if(select < 1 || select > 4) {
-					throw new RuntimeException();
-				}
-				
-				break;
-			} catch(NumberFormatException e) {
-				System.out.println("≫ 형식이 맞지 않습니다. 다시 입력해주세요.");
-			} catch(RuntimeException e) {
-				System.out.println("≫ 1 부터 4 사이의 숫자만 입력해주세요.");
+			switch(_case) {
+				case 1: calc = new Addition(a, b); break;
+				case 2: calc = new Subtraction(a, b); break;
+				case 3: calc = new Multiplication(a, b); break;
+				case 4: calc = new Division(a, b); break;
+			}
+			
+			System.out.println("\n===============================================\n");
+			
+			calc.printHeader();
+			calc.calculate();
+			
+			System.out.print("\n");
+			if(_case >= 4) {
+				System.out.println("ALU Simulator 프로그램을 종료합니다.");
+				return;
+			} else {
+				System.out.println("엔터(Enter)를 입력하면 다음 연산을 수행합니다.");
+				scan.nextLine();
+				_case += 1;
 			}
 		}
-		
-		switch(select) {
-			case 1: calc = new Addition(a, b); break;
-			case 2: calc = new Subtraction(a, b); break;
-			case 3: calc = new Multiplication(a, b); break;
-			case 4: calc = new Division(a, b); break;
-		}
-		
-		System.out.println("\n===============================================\n");
-		
-		calc.printHeader();
-		calc.calculate();
 	}
 	
 	protected static void printHeader() {
@@ -65,11 +62,5 @@ public class Application {
 		System.out.println(" /\\  |    |  |    /__` |  |\\/| |  | |     /\\   |  /  \\ |__) ");
 		System.out.println("/~~\\ |___ \\__/    .__/ |  |  | \\__/ |___ /~~\\  |  \\__/ |  \\ ");
 		System.out.println("");
-	}
-	protected static void printExpressionQuestion(int a, int b) {
-		System.out.print("① " + a + " + " + b + "   ");
-		System.out.print("② " + a + " - " + b + "   ");
-		System.out.print("③ " + a + " × " + b + "   ");
-		System.out.println("④ " + a + " / " + b + "   ");
 	}
 }
