6d1bc19ca5850402e7928afc50e4449695633e9c
이동현 <dlehdanakf@konkuk.ac.kr>
2019-10-02 23:50:43 +0900

    while 반복문을 통해 사용자가 계속해서 숫자입력이 가능하도록 수정

--- Application.java
+++ Application.java
@@ -3,13 +3,34 @@ import ALU.*;
 
 public class Application {
 	public static void main(String[] args) {
+		Application.printHeader();
+		
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String answer;
+		boolean _continue = true;
+		while(_continue) {
+			Application.run();
+			
+			System.out.println("\n===============================================\n");
+			System.out.println("계속해서 다른 숫자를 입력하시려면 Y 를 입력하세요.");
+			answer = scan.nextLine();
+			
+			if(answer.length() == 0 || ( answer.charAt(0) != 'Y' && answer.charAt(0) != 'y' )) {
+				System.out.println("ALU Simulator 프로그램을 종료합니다.");
+				return;
+			} else {
+				System.out.print("\n");
+			}
+		}
+	}
+	
+	protected static void run() {
 		int a, b;
 		Calculator calc = null;
 		
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-
-		Application.printHeader();
 		
 		System.out.println("≫ 숫자(A)를 입력해주세요.");
 		while(true) {
@@ -47,7 +68,6 @@ public class Application {
 			
 			System.out.print("\n");
 			if(_case >= 4) {
-				System.out.println("ALU Simulator 프로그램을 종료합니다.");
 				return;
 			} else {
 				System.out.println("엔터(Enter)를 입력하면 다음 연산을 수행합니다.");
@@ -56,7 +76,6 @@ public class Application {
 			}
 		}
 	}
-	
 	protected static void printHeader() {
 		System.out.println("                   __                         ___  __   __  ");
 		System.out.println(" /\\  |    |  |    /__` |  |\\/| |  | |     /\\   |  /  \\ |__) ");
